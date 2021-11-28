44518120b0ccd67496f643c1a5444a1eb8f7e826
Sufiyan Janvekar <67913416+Sufiyan-J@users.noreply.github.com>
2021-11-29 00:30:38 +0530

    src files added

---
+++ src/App.js
@@ -0,0 +1,94 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './MyComponents/Header';
+import { Footer } from './MyComponents/Footer';
+import { AddTodo } from './MyComponents/AddTodo';
+import { Todos } from './MyComponents/Todos';
+import { About } from './MyComponents/About';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+function App() {
+
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    console.log("Im onDelete of todo", todo);
+    // Deleting element in react does not work in this way
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding a todo:", title, desc);
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    } else {
+      let sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+
+  }
+  
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return (
+    <>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/" render={() => {
+            return (
+              <>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              </>
+            )
+          }}
+          >
+          </Route>
+
+          <Route exact path="/about" >
+            {<About />}
+          </Route>
+
+        </Switch>
+
+
+        <Footer />
+      </Router>
+    </>
+
+  );
+}
+
+export default App;
